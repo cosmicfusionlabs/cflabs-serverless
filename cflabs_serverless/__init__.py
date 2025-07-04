12b101b335287dd5ccedd1857d9ba24db307eba0
Amit <hello@cosmicfusionlabs.com>
2025-07-04 13:10:28 +0530

    5

--- cflabs_serverless/__init__.py
+++ cflabs_serverless/__init__.py
@@ -1,8 +1,8 @@
 """cflabs-serverless: Deploy Flask apps to AWS Lambda + API Gateway with zero code changes."""
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __author__ = "CosmicFusionLabs"
-__email__ = "info@cosmicfusionlabs.com"
+__email__ = "hello@cosmicfusionlabs.com"
 
 from .cli import app
 
